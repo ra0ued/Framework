f39108b079bf6143643ff627e9636b9b5b625bf1
Redjik <Redjiks@gmail.com>
2016-07-25 00:48:45 +0900

    First test added

--- src/app.php
+++ src/app.php
@@ -27,7 +27,7 @@ class LeapYearController
 
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => array(new LeapYearController(), 'indexAction'),
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
 
 return $routes;
